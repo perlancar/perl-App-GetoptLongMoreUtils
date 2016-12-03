78c3b24f9d886bd2815c5892c8a8dac262a5991d
perlancar (@pc-home) <perlancar@gmail.com>
2016-12-03 13:17:54 +0700

    Initial commit

---
+++ lib/App/GetoptLongMoreUtils.pm
@@ -0,0 +1,21 @@
+package App::GetoptLongMoreUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Collection of CLI utilities related to Getopt::Long::More
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+L<Getopt::Long::More>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Getopt::Long::More>
+
+=cut
